edbb69f1e58f74734506cc29eaedb28c8e0bc55f
Bradley McGill <bradley@bradleymcgill.co.uk>
2021-10-14 21:13:55 +0100

    Mainly completed Tarifa
    I have a bug if they're equal and I may have misunderstood the task so I will come back to this.

---
+++ Tarifa/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number of megabites available each month (0<X<100): ");
+            int perMonth = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the number of months passed (0<N<100): ");
+            int monthsPassed = int.Parse(Console.ReadLine());
+
+            int[] used = new int[monthsPassed];
+
+            for (int x = 0;x<monthsPassed;x+=1){
+                Console.Write("Please enter the number GB used in month " + (1+x).ToString() + ": ");
+                used[x] = int.Parse(Console.ReadLine());
+            }
+
+            while ((perMonth * monthsPassed) <= (used[0] + used[1] + used[2])){
+            
+                for (int x = 0;x<monthsPassed;x+=1){
+                    Console.Write("Please enter the number GB used in month " + (1+x).ToString() + ": ");
+                    used[x] = int.Parse(Console.ReadLine());
+                }
+            }
+            
+            int nextMonth = (perMonth * monthsPassed) - (used[0] + used[1] + used[2]) + perMonth;
+
+            Console.WriteLine(nextMonth);
+        }
+    }
+}
