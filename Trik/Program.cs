04a245e7ef16f2714d19eb2bfbfc7fd653812798
Bradley McGill <bradley@bradleymcgill.co.uk>
2021-10-14 20:35:50 +0100

    In progress of Trik just finished function
    Wrote my first function in c# to help with the swapping of the cups

---
+++ Trik/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Trik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] cups = new int[3] {1,0,0};
+            int[] Swapper(int to, int from){
+
+                int temp = cups[to];
+                cups[to] = cups[from];
+                cups[from] = temp;
+
+                return cups;
+            }
+
+
+
+            Console.WriteLine(@"You will be asked to enter your moves. 
+Enter 'A' if you want to swap cup 1 and 2.
+Enter 'B' if you want to swap cup 2 and 3.
+Enter 'C' if you want to swap cup 1 and 3.
+You can enter up to 50 moves in one line.
+The ball starts in the leftmost cup (1)");
+            Console.Write("Please enter your moves: ");
+            string moves = Console.ReadLine();
+
+           foreach (char v in moves) {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
