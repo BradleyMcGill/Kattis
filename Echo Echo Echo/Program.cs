f22eb8f81cfaae26f69929afdb471e1cd73964b5
Bradley McGill <bradley@bradleymcgill.co.uk>
2021-10-14 19:59:18 +0100

    Completed Echo Echo Echo
    Learnt that you cannot use multiply on strings to repeat them as you can in Python.

---
+++ Echo Echo Echo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Echo_Echo_Echo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word to Echo: ");
+            string word = Console.ReadLine() + " ";
+
+            Console.WriteLine(word + word + word);
+        }
+    }
+}
