e81039792d25ddb229fd61577d5eedf32f61b1bf
Bradley McGill <bradley@bradleymcgill.co.uk>
2021-10-15 23:28:36 +0100

    Completed FizzBuzz
    I think this could be done better/more efficiently but  I'm not aware of how at the moment maybe I will return to this one later

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+
+            Console.Write("Please enter the divisor you would like to replace with \"Fizz\": ");
+            int fizz = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the divisor you would like to replace with \"Buzz\": ");
+            int buzz = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the number you would like to run until: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int x = 1; x <= count; x += 1){
+                output = "";
+               if (x % fizz == 0){
+                   output += "Fizz";
+               }
+               if (x % buzz == 0){
+                   output += "Buzz";
+               }
+               if (!(x % fizz == 0) && !(x % buzz == 0)) {
+                   output = x.ToString();
+               }
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
