fc5a1c60558d266b04b0ca8d2541e81c8e371f2e
Bradley McGill <bradley@bradleymcgill.co.uk>
2021-10-17 00:46:07 +0100

    Completed Aaah!
    This one wasn't that hard, just comparing then length of two strings. This was the first time I have had to find the length of a string in C# though!

---
+++ Aaah!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aaah_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the length of the \"aaah\" youre willing to do today (0-999 'a's followed by a single 'h')");
+            string acceptable = Console.ReadLine();
+
+            Console.WriteLine("Please enter the length of the \"aaah\" the doctor is asking for (0-999 'a's followed by a single 'h')");
+            string required = Console.ReadLine();
+
+            if (acceptable.Length >= required.Length){
+                Console.WriteLine("go");
+            }
+            else {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
