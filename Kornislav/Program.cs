2b96bdc9dd4320a9c918f34b66d7460167c47bb4
Bradley McGill <bradley@bradleymcgill.co.uk>
2021-10-17 00:05:24 +0100

    Completed Kornislav
    I'm not sure if this is how they intended it to be solved but this worked for me and I don't see any logical errors made.

---
+++ Kornislav/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kornislav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter four posative intergers seperated by spaces: ");
+            int[] lengths = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+
+            Array.Sort(lengths);
+
+            Console.Write("The largest rectangle you can make with these is " + (lengths[0] * lengths[2]).ToString());
+
+        }
+    }
+}
